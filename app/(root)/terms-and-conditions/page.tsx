742c24988b78e10b700add34d734fb9df5c11267
Usman Ghafoor <uk291244@gmail.com>
2025-07-12 02:47:22 +0500

    feat: add privacy policy and terms of use pages with structured content for user information and compliance

---
+++ app/(root)/terms-and-conditions/page.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-10 max-w-7xl mx-auto layout">
+      <h1 className="text-3xl font-bold mb-6">Terms of Use</h1>
+
+      <section className="space-y-4 text-gray-800">
+        <p>
+          We strive for 100% Customer Satisfaction. If you are not completely
+          satisfied with your products or Service, please contact us
+          immediately.
+        </p>
+
+        <p>
+          You will retain ownership of any submitted Designs, and you grant us
+          and our designees a worldwide, non-exclusive, transferable,
+          royalty-free, perpetual irrevocable right and license, with right of
+          sublicense (through multiple tiers), to use, reproduce, distribute
+          (through multiple tiers), create derivative works of and publicly
+          display such Designs solely in connection with the production or
+          provision of any product or service you request or to show you how
+          your Designs would appear in our products or services. For example,
+          when you place an order for a product, we will prepare, manipulate (if
+          necessary), and transmit the Design for production, packaging and
+          shipment. In addition, you grant a nonexclusive, worldwide,
+          transferable, sublicensable right to use, reproduce, publicly display,
+          sell, and distribute the Design in advertising, marketing, samples,
+          and promotional materials for the purpose of promoting the Site.
+          Without limitation, this promotion, marketing, or advertising may
+          consist of: (i) display of your products; (ii) promotional
+          &quot;streams&quot; of audio/video/photographic content on the Site,
+          (iii) Design placement in magazines, television shows, movies, and
+          other media; and (iv) the promotion of the Site through third party
+          feeds.
+        </p>
+
+        <p>
+          Please note that, while you retain ownership of your Design, this Site
+          contains graphics, photographs, images, document layouts, templates,
+          artwork, text, fonts, software tools, and other information (referred
+          to herein as &quot;Hot Market Design And Distribution Content&quot;).
+          This Site and all Hot Market Design And Distribution Content are the
+          copyrighted property of Hot Market Design And Distribution and/or its
+          subsidiaries or the copyrighted property of parties from whom Hot
+          Market Design And Distribution has licensed such property. All rights
+          in the Site and the Hot Market Design And Distribution Content are
+          reserved worldwide. It is strictly prohibited to retain, copy,
+          distribute, publish, or use any portion of the Hot Market Design And
+          Distribution Content except as expressly allowed in this Policy. Hot
+          Market Design And Distribution reserves the right to add to, delete
+          from, or modify any part of the Hot Market Design And Distribution
+          Content at any time without prior notice. Any modifications to the Hot
+          Market Design And Distribution Content remain the property of Hot
+          Market Design And Distribution and its licensors.
+        </p>
+
+        <p>
+          You represent and warrant that you own or otherwise possess all
+          necessary rights with respect to the Designs, and that the Designs do
+          not and will not:
+        </p>
+        <ul className="list-disc pl-6">
+          <li>
+            a. infringe, misappropriate, use or disclose without authorization
+            or otherwise violate any copyright, trademark, service mark, trade
+            secret right or other intellectual property or other property right
+            of any third party;
+          </li>
+          <li>
+            b. invade or otherwise violate the privacy and/or publicity rights
+            of any individual;
+          </li>
+          <li>
+            c. violate any laws or possesses or transmit fraudulent,
+            threatening, abusive, libelous, defamatory, obscene or otherwise
+            objectionable materials.
+          </li>
+        </ul>
+
+        <p>
+          You also represent that there are no outstanding disputes in
+          connection with the property rights, intellectual property rights or
+          other rights in the Design or any part of the Design.
+        </p>
+
+        <p>
+          You consent to the use of your likeness, and you have obtained the
+          written consent, release, and/or permission of every identifiable
+          individual who appears in any submitted Design to use such
+          individual&apos;s likeness, for purposes of using and otherwise
+          exploiting the Design in the manner contemplated by this Policy, or,
+          if any such identifiable individual is under the age of eighteen (18),
+          you have obtained such written consent, release and/or permission from
+          such individual&apos;s parent or guardian (and you agree to provide to
+          us a copy of any such consents, releases and/or permissions upon our
+          request). If you do submit a submission that contains the likeness of
+          an identifiable individual under the age of eighteen (18), we strongly
+          encourage you not to include any identifying information (such as the
+          individual&apos;s name or address) with such Design.
+        </p>
+
+        <p>
+          All Designs that you upload are automatically stored by Hot Market
+          Design And Distribution. Hot Market Design and Distribution does not
+          review any Designs before you upload them to the Site. As such,
+          uploading a Design to the Site does not mean that such Design is in
+          compliance with this Policy. By uploading Designs to the Site, you
+          assume full responsibility for all such Designs and for compliance
+          with this Policy.
+        </p>
+
+        <p>
+          You agree that any Design submitted as part of an order for services
+          may be reviewed for compliance with this Policy and that Hot Market
+          Design And Distribution may refuse to use any Design and may suspend
+          processing and shipping of any order relating to the Design. Hot
+          Market Design And Distribution and its designees reserve the right to
+          demand confirmation from you in writing of all rights, authorizations,
+          licenses, permissions, and consents owned or obtained by you (if any)
+          with respect to any Design you upload to or transmit through the Site.
+          If you fail to provide us with satisfactory confirmation upon request,
+          we reserve the right to remove or deny access to any or all of your
+          Designs available via the Site and to suspend or terminate your
+          account. In addition, you agree that Hot Market Design And
+          Distribution may disclose any Design and the circumstances surrounding
+          its submission and use to any third party in order to provide the
+          requested services, enforce this Policy or comply with any legal
+          obligations or governmental requests.
+        </p>
+
+        <p>
+          You agree to indemnify and hold us, our directors, officers,
+          shareholders, employees, contractors, agents, representatives,
+          affiliates, and third party users harmless from and against any and
+          all claims, causes of action, liabilities, damages, losses, expenses
+          and costs (including, without limitation, attorneys&apos; fees) that
+          arise directly or indirectly out of or from:
+        </p>
+        <ul className="list-disc pl-6">
+          <li>
+            a. your violation of this Policy, any other agreement or terms of
+            use with us, any representation or warranty contained herein or
+            therein or any applicable law;
+          </li>
+          <li>b. your Designs;</li>
+          <li>
+            c. your activities in connection with obtaining any products or
+            services from us,
+          </li>
+          <li>
+            d. any activity related to access to or use of your account by you
+            or any other person.
+          </li>
+        </ul>
+
+        <p>
+          Designs that violate this Policy may be removed from the Site;
+          provided, however, that we have no obligation to remove Designs in
+          response to user reports or requests. We are not responsible for, and
+          will have no liability for, the removal, non-removal or loss of any
+          Designs from our Site. We recommend you keep back-up copies of your
+          Design in your own personal storage system.
+        </p>
+
+        <p>
+          All trademarks and service marks on our Site not owned by us are the
+          property of their respective owners. The trade names, trademarks and
+          service marks owned by us, whether registered or unregistered, may not
+          be used in connection with any product or service that is not ours or
+          in any manner that is likely to cause confusion. Nothing contained on
+          the Site should be construed as granting, by implication, estoppel or
+          otherwise, any license or right to use any of our trade names,
+          trademarks or service marks without our express prior written consent.
+        </p>
+
+        <p>
+          <a
+            href="https://www.copyright.gov/timeline/"
+            className="text-blue-600 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://www.copyright.gov/timeline/
+          </a>
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default page;
